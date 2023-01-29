b3d779e4686425a91093e41f4431bcd4d6bd3c79
AlphaCorvus <alphacorvus@protonmail.com>
2023-01-29 16:02:10 +0100

    [ADD] - first commit

---
+++ README.md
@@ -0,0 +1,15 @@
+# discord-songwhip
+
+A simple Discord bot that uses Songwhip to share a multi-platform music link.
+
+For a given music track, [Songwhip](https://songwhip.com/) provides a single link where you can find the track on majors platforms (Spotify, Apple Music, Tidal, and more).
+
+This bot offers the command `/music` where a user provides in the `link` argument any links from a track and return a Songwhip link.
+
+## Bot setup
+
+1. Add a bot on the desired server (https://discord.com/developers/applications).
+2. Copy `Secrets.py.example` into `Secrets.py` and fill the values:
+    - `DISCORD_TOKEN` - Token of the Discord bot;
+    - `DISCORD_GUILDID` - GuildID of the Discord server.
+3. Launch `discord-songwhip.py`.
---
+++ discord-songwhip.py
@@ -0,0 +1,41 @@
+import discord
+import Secrets
+import requests
+import json
+import validators
+
+from discord import app_commands
+
+def requests_songwhip(link):
+
+    url = 'https://songwhip.com/' # Songwhip URL
+    input = {'url': link}
+    r = requests.post(url, json = input)
+    json_data = json.loads(r.text)
+    songwhip_link = json_data['url']
+    print(songwhip_link)
+
+    return songwhip_link
+
+
+def discord_songwhip():
+    intents = discord.Intents.default()
+    client = discord.Client(intents=intents)
+    tree = app_commands.CommandTree(client)
+    @tree.command(name = "music", description = "Share a multi-platform music track!", guild=discord.Object(id=Secrets.DISCORD_GUILDID))
+    @app_commands.describe(link="Provide a link from a song (.e.g YouTube, Spotify, Tidal link)")
+    async def music(interaction, link: str):
+        if validators.url(link):
+            #await interaction.response.send_message('Yes.')
+            await interaction.response.send_message(requests_songwhip(link))
+        else:
+            await interaction.response.send_message("I am terribly sorry but this is not a link! Post a link, any link!\nIt could be a YouTube link or a Spotify link for instance.")
+    @client.event
+    async def on_ready():
+        await tree.sync(guild=discord.Object(id=Secrets.DISCORD_GUILDID))
+        print("Ready!")
+
+    client.run(Secrets.DISCORD_TOKEN)
+
+if __name__ == '__main__':
+    discord_songwhip()
